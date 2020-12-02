8c8c4d0725718ce116785a72db58a0a6b84a456a
Neil Mispelaar <neil.mispelaar@outlook.com>
2020-12-01 22:11:07 -0500

    Add comments

--- blog/.vitepress/theme/index.js
+++ blog/.vitepress/theme/index.js
@@ -1,11 +1,15 @@
+// App styles
 import './styles/main.css'
 
+// Default layout for the app
 import Layout from './Layout.vue'
-import NotFound from './NotFound.vue'
+
+// There is currently an issue rendering custom 404 pages
+// import NotFound from './NotFound.vue'
 
 export default {
   Layout,
-  NotFound, //() => 'custom 404', // <- this is a Vue 3 functional component
+  NotFound: () => 'custom 404', // <- this is a Vue 3 functional component
   enhanceApp({ app, router, siteData }) {
     // app is the Vue 3 app instance from `createApp()`. router is VitePress'
     // custom router. `siteData`` is a `ref`` of current site-level metadata.
