001b3ed60b0b1c3b5de63d9510daa54d72993692
Neil Mispelaar <neil.mispelaar@outlook.com>
2020-12-05 15:45:26 -0500

    style: centre article content

--- docs/.vitepress/theme/components/Article.vue
+++ docs/.vitepress/theme/components/Article.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="container">
-    <div class="w-2/3">
+    <div class="sm:w-4/5 md:w-2/3 xl:w-1/2 2xl:w-1/2 mx-auto">
       <Content/><!-- make sure to include markdown outlet -->
     </div>
   </div>
