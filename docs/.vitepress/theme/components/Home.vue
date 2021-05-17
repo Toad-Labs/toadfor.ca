4e9a4993ca12ba518fd55527228c1acd3fa686da
Neil Mispelaar <neil.mispelaar@outlook.com>
2021-05-17 16:41:39 -0400

    Update Home.vue

--- docs/.vitepress/theme/components/Home.vue
+++ docs/.vitepress/theme/components/Home.vue
@@ -4,16 +4,19 @@
 
     <ul>
 
-      <li>
-        <a
-          class=" bg-gray-300 rounded-lg block my-10 p-5"
-          href="/blog/why-we-built-a-fake-chatbot/index.html">Why we built a fake chatbot</a>
-      </li>
+      
 
       <li>
         <a
           class=" bg-gray-300 rounded-lg block my-10 p-5"
           href="/blog/speculative-design-sprint-day-7-8-user-testing/index.html">Speculative Design Sprint day 7 &amp; 8: User Testing</a>
+      </li>
+      
+      
+      <li>
+        <a
+          class=" bg-gray-300 rounded-lg block my-10 p-5"
+          href="/blog/why-we-built-a-fake-chatbot/index.html">Why we built a fake chatbot</a>
       </li>
 
 
